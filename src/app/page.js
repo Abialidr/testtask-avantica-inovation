a0254ce597620491987aaf72e74913d23514755e
Abialidr <luckybelt79@gmail.com>
2023-11-04 19:14:38 +0530

    final commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,44 @@
 import Image from 'next/image';
+import { Raleway, Poppins } from 'next/font/google';
+
 import styles from './page.module.css';
+
+const raleway = Raleway({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+const poppins = Poppins({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
 export default function Home() {
   return (
     <main>
       <section className={styles.main}>
         <div className={styles.section}>
           <div className={styles.left}>
-            <h3>Explain To You How All This</h3>
-            <h2>
+            <h3 className={poppins.className}>Explain To You How All This</h3>
+            <h2 className={raleway.className}>
               Business Value Through
-              <span>Digital Products</span>
+              <span> Digital Products</span>
             </h2>
-            <p>
+            <p className={raleway.className}>
               But I must explain to you how all this mistaken idea of denouncing
               pleasure and praising pain was born and I will give you a complete
               account of the.
             </p>
-            <button>Show More</button>
-            <div></div>
+            <button className={raleway.className}>Show More</button>
+            <div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
           </div>
           <div className={styles.right}>
             <Image
