c76cc16ded17cf0089a7045c24bf0e8d25974455
Abialidr <luckybelt79@gmail.com>
2023-11-04 18:41:29 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'https://demo.phlox.pro',
+      'https://demo.phlox.pro/agency-digital-2/wp-content/uploads/sites/260/2020/12/',
+      'demo.phlox.pro',
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'demo.phlox.pro',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,51 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
+import Image from 'next/image';
+import styles from './page.module.css';
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <main>
+      <section className={styles.main}>
+        <div className={styles.section}>
+          <div className={styles.left}>
+            <h3>Explain To You How All This</h3>
+            <h2>
+              Business Value Through
+              <span>Digital Products</span>
+            </h2>
+            <p>
+              But I must explain to you how all this mistaken idea of denouncing
+              pleasure and praising pain was born and I will give you a complete
+              account of the.
+            </p>
+            <button>Show More</button>
+            <div></div>
+          </div>
+          <div className={styles.right}>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+              src={
+                'https://demo.phlox.pro/agency-digital-2/wp-content/uploads/sites/260/2020/12/Polygon-hero-right.svg'
+              }
+              height={68.5}
+              width={68.5}
+              className={styles.firstImage}
+            ></Image>
+            <Image
+              src={
+                'https://demo.phlox.pro/agency-digital-2/wp-content/uploads/sites/260/2020/12/Polygon-hero-right.svg'
+              }
+              height={41.61}
+              width={41.61}
+              className={styles.secondImage}
+            ></Image>
+            {/* <img
+              src={
+                'https://demo.phlox.pro/agency-digital-2/wp-content/uploads/sites/260/2020/12/hero-img.jpg'
+              }
+              className={styles.thirdImage}
+            ></img> */}
+            <div className={styles.rightdiv}> helo</div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
-  )
+  );
 }
